38cb1eeec2ab79cc4340f9b154f871662a8e7c44
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-08-12 10:39:11 -0400

    Firestore database seeded

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,10 +2,23 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {};
+import { seedDatabase } from '../seed.js';
+
+const config = {
+    apiKey: "AIzaSyCCr9NrCkdIJO0XsdnVNdrdpsnK_nSgk90",
+    authDomain: "instagram-59020.firebaseapp.com",
+    projectId: "instagram-59020",
+    storageBucket: "instagram-59020.appspot.com",
+    messagingSenderId: "186037011641",
+    appId: "1:186037011641:web:ff66adf0bbca0e1e4b0768"
+};
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
 
-export { firebase, FieldValue };
+// call the seed file only once
+seedDatabase(firebase);
 
+console.log('firebase', firebase);
+
+export { firebase, FieldValue };
