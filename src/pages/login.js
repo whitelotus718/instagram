a206b6e8eb8b2d8a43dbcdd1f076c350f402369a
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-08-16 17:58:46 -0400

    login page finished

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,7 +7,7 @@ export default function Login(){
     const {firebase} = useContext(FirebaseContext)
 
     const [emailAddress, setEmailAddress] = useState('');
-    const [password, usePassword] = useState('');
+    const [password, setPassword] = useState('');
 
     const [error, setError] = useState('');
     const isInvalid = password === '' || emailAddress === '';
@@ -28,19 +28,43 @@ export default function Login(){
                 />
             </div>
             <div className="flex flex-col w-2/5">
-                <h1 className="flex justify-center w-full">
-                    <img src="/images/logo.png" alt="Instagram" className="" />
-                </h1>
-                {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
-                <form onSubmit={handleLogin} method="POST">
-                    <input
-                        aria-label="Enter your email address"
-                        type="text"
-                        placeholder="Email address"
-                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
-                        border border-gray primary rounded mb-2"
-                    />
-                </form>
+                <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+                    <h1 className="flex justify-center w-full">
+                        <img src="/images/logo.png" alt="Instagram" className="" />
+                    </h1>
+                    {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+                    <form onSubmit={handleLogin} method="POST">
+                        <input
+                            aria-label="Enter your email address"
+                            type="text"
+                            placeholder="Email address"
+                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
+                            border border-gray primary rounded mb-2"
+                            onChange={({ target }) => setEmailAddress(target.value)}
+                        />
+                        <input
+                            aria-label="Enter your password"
+                            type="password"
+                            placeholder="Password"
+                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
+                            border border-gray primary rounded mb-2"
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                        <button 
+                            disabled={isInvalid}
+                            type="submit"
+                            className={
+                                `bg-blue-500 text-white w-full rounded h-8 font-bold 
+                                ${isInvalid && 'opacity-50'}`}
+                        >
+                                Log In
+                        </button>
+                    </form>
+                </div>
+                <div className="flex justify-center items-center flex-col w-full bg-white p-4 
+                border border-gray-primary">
+                    <p className="text-sm">Don't have an account?{` `}</p>
+                </div>
             </div>
         </div>
     )
