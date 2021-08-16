092210df26817eb7b42094869ddbc417a8d6a4cb
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-08-16 17:14:01 -0400

    instgram photo + logo

--- src/pages/login.js
+++ src/pages/login.js
@@ -20,7 +20,19 @@ export default function Login(){
 
     return(
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-            <p>I have no idea!</p>
+            <div className="flex w-3/5">
+                <img 
+                src="/images/iphone-with-profile.jpg" 
+                alt="iphone with Instagram"
+                className="max-w-full"
+                />
+            </div>
+            <div className="flex flex-col w-2/5">
+                <h1 className="flex justify-center w-full">
+                    <img src="/images/logo.png" alt="Instagram" className="" />
+                </h1>
+                {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+            </div>
         </div>
     )
     
