0ac2abe66f725e08b028d0b4736a2c1248da5111
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-08-16 17:19:46 -0400

    added input + tailwind styles

--- src/pages/login.js
+++ src/pages/login.js
@@ -32,6 +32,15 @@ export default function Login(){
                     <img src="/images/logo.png" alt="Instagram" className="" />
                 </h1>
                 {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+                <form onSubmit={handleLogin} method="POST">
+                    <input
+                        aria-label="Enter your email address"
+                        type="text"
+                        placeholder="Email address"
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
+                        border border-gray primary rounded mb-2"
+                    />
+                </form>
             </div>
         </div>
     )
