0dc37181012841d8c5742921f231f1c0425963be
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-08-18 09:37:25 -0400

    user auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+// import Dashboard from './pages/dashboard';
 
 const Login = lazy( () => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/sign-up'));
 const NotFound = lazy(() => import('./pages/not-found'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
           <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
           <Route component={NotFound} />
         </Switch>
       </Suspense>
