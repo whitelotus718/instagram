1c1ef86f1fed6ded116a3663663f6c685b818f35
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-08-18 00:05:36 -0400

    sign up + login Link goes to proper route

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -7,13 +7,15 @@ export default function Login(){
     const history = useHistory();
     const {firebase} = useContext(FirebaseContext)
 
+    const [username, setUsername] = useState('');
+    const [fullName, setFullName] = useState('');  
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
 
     const [error, setError] = useState('');
     const isInvalid = password === '' || emailAddress === '';
 
-    const handleLogin = async (event) => {
+    const handleSignUp = async (event) => {
         event.preventDefault();
     
         try {
@@ -45,37 +47,52 @@ export default function Login(){
                         <img src="/images/logo.png" alt="Instagram" className="" />
                     </h1>
                     {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
-                    <form onSubmit={handleLogin} method="POST">
+                    <form onSubmit={handleSignUp} method="POST">
                         <input
-                            aria-label="Enter your email address"
-                            type="text"
-                            placeholder="Email address"
-                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
-                            border border-gray primary rounded mb-2"
-                            onChange={({ target }) => setEmailAddress(target.value)}
+                        aria-label="Enter your username"
+                        type="text"
+                        placeholder="Username"
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                        onChange={({ target }) => setUsername(target.value)}
+                        value={username}
                         />
                         <input
-                            aria-label="Enter your password"
-                            type="password"
-                            placeholder="Password"
-                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
-                            border border-gray primary rounded mb-2"
-                            onChange={({ target }) => setPassword(target.value)}
+                        aria-label="Enter your full name"
+                        type="text"
+                        placeholder="Full name"
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                        onChange={({ target }) => setFullName(target.value)}
+                        value={fullName}
                         />
-                        <button 
-                            disabled={isInvalid}
-                            type="submit"
-                            className={
-                                `bg-blue-medium text-white w-full rounded h-8 font-bold 
-                                ${isInvalid && 'opacity-50'}`}
+                        <input
+                        aria-label="Enter your email address"
+                        type="text"
+                        placeholder="Email address"
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                        onChange={({ target }) => setEmailAddress(target.value)}
+                        value={emailAddress}
+                        />
+                        <input
+                        aria-label="Enter your password"
+                        type="password"
+                        placeholder="Password"
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                        onChange={({ target }) => setPassword(target.value)}
+                        value={password}
+                        />
+                        <button
+                        disabled={isInvalid}
+                        type="submit"
+                        className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+                        ${isInvalid && 'opacity-50'}`}
                         >
-                                Log In
+                        Sign Up
                         </button>
                     </form>
                 </div>
                 <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
                     <p className="text-sm">Don't have an account?{` `}
-                        <Link to="/signup" className="font-bold text-blue-medium">
+                        <Link to="/sign-up" className="font-bold text-blue-medium">
                             Sign Up
                         </Link>
                     </p>
